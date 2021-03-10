ef68083d4dde4daa650a5035f66eb7899566a73d
alexandrehutter <alexandre.hutter@epfl.ch>
2021-03-10 15:57:24 +0100

    Added up-to-date user infos from twitter API

--- Backend/tcb/tcb/views.py
+++ Backend/tcb/tcb/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
+import requests
 import pymongo
 from datetime import datetime
 
 client = pymongo.MongoClient('mongodb://localhost:27017')
 database = client['trollcheck']
+bearer_token = "AAAAAAAAAAAAAAAAAAAAABkYNgEAAAAA9gybLao9r2LyuBNHAOGDlOOivS0%3DOF9mpuGxGjJTiF1T151P5XOKHg0sAnnj05TUBbHM3VZrkv9UaS"
 
 def home(request):
 	context = {
@@ -18,16 +20,51 @@ def user(request, usr_id):
 	user = database.get_collection("users").find_one({ "_id": usr_id })
 	if not user is None:
 		details = user['details']
+
+		# Load user info from database : 
+		name = details['name']
+		screen_name = details['screen_name']
+		location = details['location']
+		tweet_count = details['statuses_count']
+		following_count = details['friends_count'],
+		followers_count = details['followers_count'],
+		likes_count = details['favourites_count'],
+		bio = details['description'],
+		pp_url = details['profile_image_url_https']
+
+		# Get updated informations from twitter API
+		headers = {"Authorization": "Bearer {}".format(bearer_token)}
+		url = "https://api.twitter.com/1.1/users/show.json?user_id={}".format(details["id"])
+		user_request =  requests.request("GET", url, headers=headers)
+
+		if(user_request.status_code == 200):
+			print("Twitter API user info request : status OK (200)")
+			user_infos = user_request.json()
+
+			name = user_infos['name']
+			screen_name = user_infos['screen_name']
+			location = user_infos['location']
+			tweet_count = user_infos['statuses_count']
+			following_count = user_infos['friends_count']
+			followers_count = user_infos['followers_count']
+			likes_count = user_infos['favourites_count']
+			bio = user_infos['description']
+			pp_url = user_infos['profile_image_url_https'].replace("_normal", "")
+		else:
+			print("Twitter API user info request error: code {}".format(user_request.status_code))
+
+
+
 		context = {
-			'name': details['name'],
-			'username': details['screen_name'],
-			'location': details['location'],
-			'tweet_count': details['statuses_count'],
-			'following_count': details['friends_count'],
-			'followers_count': details['followers_count'],
-			'likes_count': details['favourites_count'],
-			'bio': details['description'],
-			'pp_url': details['profile_image_url_https'],
+			'name': name,
+			'username': screen_name,
+			'location': location,
+			'tweet_count': tweet_count,
+			'following_count': following_count,
+			'followers_count': followers_count,
+			'likes_count': likes_count,
+			'bio': bio,
+			'pp_url': pp_url,
 			'history': json.dumps(user['history']),
 			'url': details['url'],
 			'created_at': datetime.strptime(details['created_at'], '%a %b %d %X %z %Y').strftime("%a %b %d %Y at %X"),
