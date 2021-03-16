0553f3713f99284a1ce13f3398dba3db7b8df9dd
ProtectedVariable <thomas.ibanez33@gmail.com>
2021-03-16 16:24:39 +0100

    Some peak detection

--- Backend/tcb/tcb/views.py
+++ Backend/tcb/tcb/views.py
@@ -4,6 +4,8 @@ import json
 import requests
 import pymongo
 from datetime import datetime
+import numpy as np
+import functools 
 
 client = pymongo.MongoClient('mongodb://localhost:27017')
 database = client['trollcheck']
@@ -15,6 +17,41 @@ def home(request):
 		# 'site_name' => 'TrollCheck'
 	}
 	return render(request, 'index.html', context=context)
+
+def find_peaks(history, metric):
+
+	windows = []
+	done = False
+	window_start = 0
+	window_end = 1
+	while not done: #split the history into 7-days windows
+		start = datetime.fromtimestamp(history[window_start]['ts'])
+		while window_end < len(history)-1 and (datetime.fromtimestamp(history[window_end]['ts']) - start).days < 7:
+			window_end += 1
+
+		if window_end - window_start >= 4: #ignore windows that have too few points, might not be the best thing to do tho
+			windows.append((window_start, window_end))
+		window_start = window_end 
+		window_end = window_start + 1
+		if window_end >= len(history) - 1:
+			done = True
+	
+	print(windows)
+
+	deltas = []
+	for (s,e) in windows: #compute how the 'derivative' changes between the start of the window and the end
+		delta_s = (history[s+1]['details'][metric] - history[s]['details'][metric]) / (history[s+1]['ts']-history[s]['ts'] +0.00000001)#this is just to avoid div-by-0
+		delta_e = (history[e]['details'][metric] - history[e-1]['details'][metric]) / (history[e]['ts']-history[e-1]['ts']+0.00000001)
+		deltas.append(abs(delta_e-delta_s))
+	print(deltas)
+
+	peaks_ts = []
+	for i in range(10):
+		j = np.argmax(deltas)
+		print(datetime.fromtimestamp(history[(windows[j][0])]['ts']))
+		deltas[j] = 0
+		peaks_ts.append(history[(windows[j][0])]['ts'])
+	return peaks_ts
 
 def user(request, usr_id):
 	user = database.get_collection("users").find_one({ "_id": usr_id })
@@ -55,7 +92,16 @@ def user(request, usr_id):
 		else:
 			print("Twitter API user info request error: code {}".format(user_request.status_code))
 
+		peaks = find_peaks(user['history'], 'followers_count')
+		tweets = list(database.get_collection("tweets").find({"user_id": user['details']['id_str']}))
+		
 
+		suspect_tweets = []
+		for p in peaks:
+			t = [] #TODO: Take like 10 tweets instead of just 1
+			t.append(functools.reduce(lambda a, b : a if abs(a['ts']-p) < abs(b['ts']-p) else b, tweets))
+			suspect_tweets.append(t)
+		
 
 		context = {
 			'name': name,
@@ -72,7 +118,8 @@ def user(request, usr_id):
 			'created_at': datetime.strptime(details['created_at'], '%a %b %d %X %z %Y').strftime("%a %b %d %Y at %X"),
 			'verified': '<i rel="tooltip" title="Verified account" class="bi bi-check2-circle"></i>' if details['verified'] else '<i rel="tooltip" title="Unverified account" class="bi bi-slash-circle"></i>',
 			'protected': '<i rel="tooltip" title="Protected account" class="bi bi-lock-fill"></i>' if details['protected'] else '<i rel="tooltip" title="Unprotected account" class="bi bi-unlock-fill"></i>',
-			#'tweets': user['tweets']
+			'tweets': suspect_tweets,
+			'peaks': peaks,
 		}
 		return render(request, 'user.html', context=context)
 	return HttpResponse("User not found :(")
