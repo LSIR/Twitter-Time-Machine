040808ab68a0a376210420ee0fad73dc55d9539d
ProtectedVariable <thomas.ibanez33@gmail.com>
2021-04-03 16:16:47 +0200

    Frontend going to d3js

--- Backend/tcb/tcb/analysis.py
+++ Backend/tcb/tcb/analysis.py
@@ -10,25 +10,24 @@ def find_peaks(history, metric):
 	f = interp1d(xs, ys) #linear interpolation of the metric(time) function
 
 	deltas = []
-	dt = (3*24*60*60) #In seconds
+	dt = (4*24*60*60) #In seconds
 	for i in range(1, len(history-2)):
 		py = math.log10(f(max(xs[0], xs[i]-dt)))
 		ny = math.log10(f(min(xs[-1], xs[i]+dt)))
 		y = math.log10(ys[i])
-		print(py, y, ny)
 		
 		slope_p = (y - py) / (dt)
 		slope_n = (ny - y) / (dt)
 		delta_time = 2*dt
 		expected_linear_value = slope_p * delta_time + py #basically ax + b
-		deltas.append((slope_n-slope_p))
-	print(deltas)
+		deltas.append(ny-expected_linear_value)
 
 	max_delta = np.max(deltas)
+	print(max_delta)
 	peaks_ts = []
 	while True:
 		j = np.argmax(deltas)
-		if deltas[j] < 0.7*max_delta:
+		if deltas[j] < 0.4 * max_delta:
 			break
 		deltas[j] = 0
 		peaks_ts.append(xs[j+1])
--- Backend/tcb/tcb/urls.py
+++ Backend/tcb/tcb/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -22,4 +24,4 @@ urlpatterns = [
     path('user/<str:usr_id>', views.user, name='user'),
     path('admin/', admin.site.urls),
     path('tweets/<str:usr_id>', views.tweets),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
