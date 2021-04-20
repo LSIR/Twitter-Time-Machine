78d8d51d87024871df9cc2582cc48406abeb7ed9
ProtectedVariable <thomas.ibanez33@gmail.com>
2021-04-20 17:52:45 +0200

    d3 ui equivalent to what we had before

--- Backend/tcb/static/tc-chart.js
+++ Backend/tcb/static/tc-chart.js
@@ -1,5 +1,7 @@
 let charts = {}
 
+const MAX_DATAPOINTS = 800
+
 function tcMakeHistoryChart(divID, raw_data, metric, xaxis, yaxis) {
 
 	let history = raw_data.map((x) => {
@@ -14,13 +16,13 @@ function tcMakeHistoryChart(divID, raw_data, metric, xaxis, yaxis) {
     	.style("opacity", 0);
 	let chart = builder
 		.setParentDiv($(divID))
-		.setData(history)
+		.setData(optimizePoints(history, MAX_DATAPOINTS, []))
 		.setFilled(true)
 		.setMargin(50,50,50,50)
 		.setXAxisScale(TIME_SCALE)
 		.setXAxisScaleDomain(SCALE_EXTENT)
 		.setYAxisScale(LINEAR_SCALE)
-		.setYAxisScaleDomain(SCALE_ZERO_MAX)
+		.setYAxisScaleDomain(SCALE_EXTENT)
 		.setHoverEvent(
 			function(d) {		
 				div.transition()		
@@ -34,22 +36,22 @@ function tcMakeHistoryChart(divID, raw_data, metric, xaxis, yaxis) {
 		.setOutEvent(
 			function(d) {		
 				div.transition()		
-					.duration(500)		
+					.duration(200)		
 					.style("opacity", 0);	
 			}
 		)
 		.build();
 	
 	let safeDivID = divID.substr(1);
-	$(divID+" > .svg-container").append('<div class="row" id="'+safeDivID+'_controls"></div>')
+	$(divID).append('<div class="row" id="'+safeDivID+'_controls"></div>')
 	let controlRow = $(divID+'_controls')
-	controlRow.append('<div class="offset-1 col-2 form-switch">\
-				<input class="form-check-input" type="checkbox" id="logscale_'+safeDivID+'">\
+	controlRow.append('<div class="col-3 form-switch">\
 				<label class="form-check-label" for="logscale_'+safeDivID+'">Logarithmic scale</label>\
+				<input class="form-check-input" type="checkbox" id="logscale_'+safeDivID+'" style="margin-left: 0">\
 				</div>')
-	controlRow.append('<div class="col-2 form-switch">\
-				<input onchange="optimizePoints(\''+divID+'\', 500)" checked class="form-check-input" type="checkbox" id="optimize_'+safeDivID+'">\
+	controlRow.append('<div class="col-3 form-switch">\
 				<label class="form-check-label" for="optimize_'+safeDivID+'">Optimize Points</label>\
+				<input checked class="form-check-input" type="checkbox" id="optimize_'+safeDivID+'" style="margin-left: 0">\
 				</div>')
 	let minDate = new Date(history[0].x).yyyymmdd()
 	let maxDate = new Date(history[history.length-1].x).yyyymmdd();
@@ -65,22 +67,38 @@ function tcMakeHistoryChart(divID, raw_data, metric, xaxis, yaxis) {
 	$('#logscale_'+safeDivID).on('click', function() {
 		if($('#logscale_'+safeDivID).prop('checked')) {
 			chart.setLogscale('y', true, SCALE_EXTENT);
-			//chart.updateData(history.map(e => ({y: Math.log10(e.y), x: e.x})), SCALE_EXTENT, SCALE_ZERO_MAX, 500);
+			//chart.updateData(history.map(e => ({y: Math.log10(e.y), x: e.x})), SCALE_EXTENT, SCALE_ZERO_MAX, MAX_DATAPOINTS);
 		} else {
 			chart.setLogscale('y', false, SCALE_EXTENT);
-			//chart.updateData(history, SCALE_EXTENT, SCALE_ZERO_MAX, 500);
+			//chart.updateData(history, SCALE_EXTENT, SCALE_ZERO_MAX, MAX_DATAPOINTS);
 		}
 	});
+
+	function updateChart(data) {
+		if($('#optimize_'+safeDivID).prop('checked')) {
+			data = optimizePoints(data, MAX_DATAPOINTS, []);
+		}
+		chart.updateData(data, SCALE_EXTENT, SCALE_EXTENT, MAX_DATAPOINTS);
+	}
 
 	$('#start'+safeDivID).on('change', function() {
 		let startDate = new Date($('#start'+safeDivID).val()).getTime();
 		let nd = history.filter(e => e.x >= startDate);
-		chart.updateData(nd, SCALE_EXTENT, SCALE_EXTENT, 500);
+		updateChart(nd);
 	})
 	$('#end'+safeDivID).on('change', function() {
 		let endDate = new Date($('#end'+safeDivID).val()).getTime();
 		let nd = history.filter(e => e.x <= endDate);
-		chart.updateData(nd, SCALE_EXTENT, SCALE_EXTENT, 500);
+		updateChart(nd);
+	})
+	$('#optimize_'+safeDivID).on('change', function() {
+		let startDate = new Date($('#start'+safeDivID).val()).getTime();
+		let endDate = new Date($('#end'+safeDivID).val()).getTime();
+		if($('#optimize_'+safeDivID).prop('checked')) {
+			updateChart(history.filter(e => e.x >= startDate && e.x <= endDate))
+		} else {
+			updateChart(history)
+		}
 	})
 }
 
