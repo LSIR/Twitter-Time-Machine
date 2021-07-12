b1a112d70b9af0cacfdd8975fa513692c9503f03
alexandrehutter <alexandre.hutter@epfl.ch>
2021-07-12 22:58:30 +0200

    Corrected deletions

--- Backend/tcb/tcb/views.py
+++ Backend/tcb/tcb/views.py
@@ -84,10 +84,14 @@ def user(request, usr_id):
 		tweets_metadata = list(database.get_collection("tweets").find({"user_id": details['id']}, {'_id': False, 'user_id': False}))
 
 		# Remove duplicate tweets
+		print(tweets_metadata)
 		tweets_metadata = pd.DataFrame(tweets_metadata)
 		temp_tweets = tweets_metadata.copy().groupby("text").last().reset_index().drop(columns=["ts"])
 		timestamps = tweets_metadata.copy()[["text", "ts"]].groupby("text").first().reset_index()
+		deleted = tweets_metadata[tweets_metadata.deleted == True]
 		tweets_metadata = temp_tweets.merge(timestamps, on="text")
+		tweets_metadata = pd.concat([tweets_metadata, deleted], axis=0)
+		tweets_metadata = tweets_metadata.fillna("")
 		
 		tweets_metadata = tweets_metadata.to_dict('records')
 
